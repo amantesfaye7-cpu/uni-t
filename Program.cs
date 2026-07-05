89bc3b3bcbb104eec8e5a81fd01223b969a549ea
amantesfaye7-cpu <amantesfaye7@gmail.com>
2026-07-04 21:01:17 -0400

    Add Program.cs entry point

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace UniT161E
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
